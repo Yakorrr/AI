c9f45218ce4445349350fbbfe1c5762eeade46ac
Yakorrr <vovkayakimenko2564@gmail.com>
2023-05-19 23:02:35 +0300

    Corridors place was finished, walls place was started

--- labs/lab3/world.py
+++ labs/lab3/world.py
@@ -5,10 +5,13 @@ class GridWorld:
     def __init__(self, width, height, bombs):
         self.width = width
         self.height = height
-        self.grid = self.fill_grid()
-        self.bombs_coordinates = self.place_bombs(bombs)
         self.room_width = self.room_height = 5
+
+        self.grid = self.fill_grid()
         self.corridors = self.place_corridors()
+        self.walls = self.place_walls()
+        # self.bombs_coordinates = self.place_bombs(bombs)
+
         self.slip_chance = 0.1
         self.probability = 1 - self.slip_chance
         self.penalty = 0.8
@@ -27,18 +30,21 @@ class GridWorld:
     def fill_grid(self):
         return [[0 for _ in range(self.width)] for _ in range(self.height)]
 
-    def place_bombs(self, amount):
-        def coordinates():
-            return random.randint(0, self.width - 1), random.randint(0, self.height - 1)
+    def random_coordinates(self):
+        return random.randint(0, self.width - 1), random.randint(0, self.height - 1)
 
+    def place_bombs(self, amount):
         bombs_coords = []
-        x, y = coordinates()
 
         for i in range(amount):
-            while self.grid[x][y] == -50:
-                x, y = coordinates()
+            x, y = self.random_coordinates()
+
+            while self.grid[x][y] == -50 and (x, y) in self.corridors:
+                x, y = self.random_coordinates()
+                # print("X: %s, y: %s" % (x, y))
 
             bombs_coords.append((x, y))
+            print("X: %s, y: %s" % (x, y))
             self.grid[x][y] = -50
 
         return bombs_coords
@@ -48,30 +54,69 @@ class GridWorld:
         current_width, current_height = self.room_width - 1, self.room_height - 1
 
         for i in range(0, self.width, self.room_width):
-            print("Before L:", i, current_height)
-
             for j in range(0, self.height, self.room_height):
-                print("Before H:", j, current_width)
+                if current_width < self.width - 1:
+                    temp_side = random.randint(i, min(current_height - 1, self.height - 1))
+                    corridors_coords.append((temp_side, current_width))
+                    self.grid[temp_side][min(current_width, self.width - 1)] = "||||"
 
-                # if j < self.width - 1:
-                temp_side = random.randint(j, current_width)
-                corridors_coords.append((temp_side, j))
-                self.grid[temp_side][j] = "||||"
-
-                # if i < self.height - 1:
-                temp_bottom = random.randint(i, current_height)
-                corridors_coords.append((i, temp_bottom))
-                self.grid[i][temp_bottom] = "||||"
+                if current_height < self.height - 1:
+                    temp_bottom = random.randint(j, min(current_width - 1, self.width - 1))
+                    corridors_coords.append((current_height, temp_bottom))
+                    self.grid[min(current_height, self.height - 1)][temp_bottom] = "||||"
 
                 current_width += self.room_width
 
             current_height += self.room_height
             current_width = self.room_width - 1
 
-        print(corridors_coords)
-
         return corridors_coords
 
+    def place_walls(self):
+        side, bottom = self.room_height, self.room_height
 
-world = GridWorld(25, 25, 10)
+        for i in range(0, self.width, self.room_width):
+            for j in range(0, self.height, self.room_height):
+                print("Side:", j, side - 1)
+                print("Bottom:", bottom - 1, i)
+
+                for vertical in range(side - self.room_height, min(side, self.height - 1)):
+                    if (vertical, bottom - 1) not in self.corridors:
+                        self.grid[vertical][bottom - 1] = "####"
+
+                for horizontal in range(bottom - self.room_width, min(bottom, self.width - 1)):
+                    if (side - 1, horizontal) not in self.corridors:
+                        self.grid[side - 1][horizontal] = "####"
+
+                bottom += self.room_width
+
+            side += self.room_height
+            bottom = self.room_width
+
+        # for i in self.corridors:
+        #     x, y = i[0], i[1]
+        #     print(x, y)
+        #
+        #     temp_x, temp_y = x % (self.room_height - 1), y % (self.room_width - 1)
+        #
+        #     if temp_x == 0 and temp_y != 0:
+        #         for j in range(bottom, self.height):
+        #             if (x, j) in self.corridors:
+        #                 continue
+        #             else:
+        #                 self.grid[x][j] = "####"
+        #
+        #         bottom = x
+        #
+        #     if temp_y == 0 and temp_x != 0:
+        #         for j in range(0, self.width):
+        #             if (j, y) in self.corridors:
+        #                 continue
+        #             else:
+        #                 self.grid[j][y] = "####"
+        #
+        #         side = y
+
+
+world = GridWorld(10, 10, 10)
 world.print_world()
